932b7c14ef78958d3b639436bd1b88617a4d1cf3
Alvin Theodora <alvintheodoraa@gmail.com>
2017-11-24 23:54:32 +0700

    make test function for app

--- app/Http/Controllers/ServiceController.php
+++ app/Http/Controllers/ServiceController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 use App\Http\Controllers\Controller;
 use App\Models\User;
+use App\Models\OrderCustomer;
 
 class ServiceController extends Controller
 {
@@ -14,10 +15,28 @@ class ServiceController extends Controller
 			switch($request->keyword){
 				case 'login':
 					return $this->login($request);
-					break;
-
+					break;				
+				
+				//////////////this part below needs to be login first//////////////
 				default:
-					return $this->apiResponse(0,'keyword salah','keyword salah');
+					if( !$this->isLogin($request->user_id,$request->token) ){
+						return $this->apiResponse(2,'Login gagal, silahkan login kembali','Invalid Ervill token');
+					}
+
+					switch($request->keyword){
+						case 'test':
+							return $this->test($request);
+							break;
+
+						case 'logout':
+							return $this->logout($request);
+							break;
+
+						default:
+							return $this->apiResponse(0,'keyword salah','keyword salah');
+							break;
+					}
+					
 					break;
 			}
 		}else{
@@ -30,6 +49,7 @@ class ServiceController extends Controller
     	$user = User::with('role')
         ->where('username', $request->username)->first();
 
+        //check if username and password is correct
     	if($user && Hash::check($request->password, $user->password)){
             $token = str_random(60);
             $data = array(
@@ -37,11 +57,40 @@ class ServiceController extends Controller
                 'user' => $user->toArray()
             );
 
+            //set token to random string
             $user->doUpdateApiToken($token);
 
             return $this->apiResponse(1,'berhasil login','',$data);
         }
-        return $this->apiResponse(0,'gagal login','');
+        return $this->apiResponse(0,'gagal login','gagal login');
+    }
+
+    public function logout($request){
+    	$user = User::where('id', $request->user_id)->first();
+
+    	if($user){
+    		//set token to null
+            $user->doUpdateApiToken(null);
+
+            $data = array(        
+           		'success' => true
+            );
+
+            return $this->apiResponse(1,'berhasil logout','',$data);
+        }
+        return $this->apiResponse(0,'gagal logout','gagal logout');
+    }
+
+    ///////////change OC status, for testing only////////////
+    public function test($request){
+    	$orderCustomer = OrderCustomer::first();
+
+    	if( $orderCustomer ){
+    		$orderCustomer->doUpdateStatus($request->status);         
+
+            return $this->apiResponse(1,'berhasil mengubah data','');
+        }
+        return $this->apiResponse(0,'gagal mengubah data','gagal mengubah data');
     }
 
     public function apiResponse($status,$message='',$error='',$data=array()){
@@ -51,6 +100,15 @@ class ServiceController extends Controller
     		'error' => $error,
     		'data' => $data
     	));
+    }
+
+    //check id and token is valid or not
+    public function isLogin($id,$token){
+    	if(!$id || !$token){
+    		return false;
+    	}
+    	$user = User::where([['id',$id],['ervill_token',$token]])->first();
+    	return $user?true:false;
     }
 
 
