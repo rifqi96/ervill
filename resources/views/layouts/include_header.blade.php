22b2ae34172be125d2afedb040c21864843b760b
Alvin Theodora <alvintheodoraa@gmail.com>
2018-04-06 13:21:34 +0700

    add highcharts in overview

--- resources/views/layouts/include_header.blade.php
+++ resources/views/layouts/include_header.blade.php
@@ -16,6 +16,7 @@
 <link rel="stylesheet" href="{{asset('assets/css/lib/font-awesome/font-awesome.min.css')}}">
 <link rel="stylesheet" href="{{asset('assets/css/lib/bootstrap/bootstrap.min.css')}}">
 <link rel="stylesheet" href="{{asset('assets/css/main.css')}}">
+
 <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/v/bs/jszip-2.5.0/dt-1.10.16/b-1.5.1/b-colvis-1.5.1/b-flash-1.5.1/b-html5-1.5.1/b-print-1.5.1/fh-3.1.3/r-2.2.1/sc-1.4.3/sl-1.2.4/datatables.min.css"/>
 <link href="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.6-rc.0/css/select2.min.css" rel="stylesheet" />
 <link rel="stylesheet" href="{{ mix('/css/all.css') }}">
